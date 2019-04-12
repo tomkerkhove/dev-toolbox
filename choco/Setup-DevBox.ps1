395a6f130150791a61ad004de3d7427b798435e8
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-04-12 10:42:44 +0200

    Introduce Setup-DevBox.ps1

---
+++ choco/Setup-DevBox.ps1
@@ -0,0 +1,46 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco upgrade chocolatey
+
+choco install linqpad -y
+choco install toggl -y
+choco install franz -y
+choco install microsoft-teams -y
+choco install notepadplusplus -y
+choco install vscode -y
+choco install googlechrome -y
+choco install ccleaner -y
+choco install spotify -y
+choco install servicebusexplorer -y
+choco install 7zip -y
+choco install kubernetes-cli -y
+choco install docker-compose -y
+choco install minikube -y
+choco install docker-kitematic -y
+choco install conemu -y
+choco install vscode-docker -y
+choco install vscode-icons -y
+choco install vscode-csharp -y
+choco install vscode-powershell -y
+choco install vscode-azurerm-tools -y
+choco install vscode-markdownlint -y
+choco install microsoftazurestorageexplorer -y
+choco install azure-cli -y
+choco install dropbox -y
+choco install firefox -y
+choco install git -y
+choco install postman -y
+choco install 1password -y
+choco install 1password-desktoplauncher -y
+choco install fiddler -y
+choco install grammarly -y
+choco install keepass -y
+choco install keepass-plugin-keeanywhere -y
+choco install winmerge -y
+choco install vlc -y
+choco install paint.net -y
+choco install lightshot -y
+choco install nugetpackageexplorer -y
+choco install nuget.commandline -y
+choco install resharper-clt.portable -y
+choco install resharpers -y
