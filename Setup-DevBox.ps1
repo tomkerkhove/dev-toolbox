19aee47abe689b855cda69a2ef7588f3d3582cec
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-04-19 11:07:15 +0200

    Stitch everything together
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- Setup-DevBox.ps1
+++ Setup-DevBox.ps1
@@ -1,2 +1,14 @@
+# Configure PowerShell
+Write-Host "Configuring PowerShell..."
+.\configure-posh\Setup-PowerShellEnvironment.ps1
+Write-Host "PowerShell Configured."
+
 # Install Bash & Linux Subsystem for Windows
+Write-Host "Installing Bash & Linux Subsystem for Windows..."
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+Write-Host "Bash & Linux Subsystem for Windows Installed."
+
+# Install apps
+Write-Host "Installing apps..."
+.\apps\Setup-DevToolbox.ps1
+Write-Host "Apps installed"
