abe9cbf788b575a693c17c7c404592a776bb8f63
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-12-09 10:16:59 +0100

    Update DeleteLocalBranches.sh

--- git/DeleteLocalBranches.sh
+++ git/DeleteLocalBranches.sh
@@ -1,2 +1,2 @@
 # Source: https://stackoverflow.com/questions/10610327/delete-all-local-git-branches
-git branch | grep -v "develop" | grep -v "master" | xargs git branch -D
+git branch | grep -v "develop" | grep -v "master" | grep -v "main" | xargs git branch -D
