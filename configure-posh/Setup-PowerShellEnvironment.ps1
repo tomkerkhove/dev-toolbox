6b8340ab04009a345bfe5364a0c65759e475562d
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-05-03 14:12:17 +0200

    Automatically copy Oh My Posh template
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- configure-posh/Setup-PowerShellEnvironment.ps1
+++ configure-posh/Setup-PowerShellEnvironment.ps1
@@ -1,4 +1,8 @@
+# Install modules
 Install-Module posh-git -Force
 Install-Module oh-my-posh -Force
 Install-Module posh-docker -Force
 Install-Module -Name Terminal-Icons -Force
+
+# Copy Oh My POSH template
+Get-Content .\.oh-my-posh.omp.json > $Home\.oh-my-posh.omp.json
