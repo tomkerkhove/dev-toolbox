ce0958e5650044192e1d3b460689a2893e332939
Tom Kerkhove <tomkerkhove@microsoft.com>
2021-11-02 15:16:21 +0100

    fix: Use Set-PoshPrompt -Theme for oh-my-posh

--- configure-posh/Setup-PowerShellEnvironment.ps1
+++ configure-posh/Setup-PowerShellEnvironment.ps1
@@ -10,7 +10,7 @@ Install-Module -Name Terminal-Icons -Force
 
 # Copy Oh My POSH template
 Write-Host "Copying oh-my-posh template..."
-Get-Content .\configure-posh\.oh-my-posh.omp.json > $Home\.oh-my-posh.omp.json
+Set-PoshPrompt -Theme .\configure-posh\.oh-my-posh.omp.json
 
 # Copy default PowerShell startup
 Write-Host "Configuring PowerShell startup..."
